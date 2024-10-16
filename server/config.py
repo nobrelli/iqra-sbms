7c1dc9f2a9aeba968c41ec5c017e509b71ded3e4
Ryu Rabino <ryuedwards@gmail.com>
2024-10-16 18:23:39 +0800

    Fixes

--- server/config.py
+++ server/config.py
@@ -24,8 +24,6 @@ class ProductionConfig(Config):
         self.JWT_COOKIE_DOMAIN = os.getenv('CLIENT_DOMAIN')
         self.JWT_COOKIE_SECURE = True
         self.JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=1)
-        
-        self.CORS_ORIGINS = os.getenv('CLIENT_SITE')
 
 
 class DevelopmentConfig(Config):
