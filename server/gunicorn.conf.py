258090106aff45ccdbe8718a2cfc3244645435aa
Ryu Rabino <ryuedwards@gmail.com>
2024-10-17 10:56:45 +0800

    Fixes

--- server/gunicorn.conf.py
+++ server/gunicorn.conf.py
@@ -1,14 +1,18 @@
 import multiprocessing, os
 from dotenv import load_dotenv
 
+DEFAULT_HOST = '0.0.0.0'
+DEFAULT_PORT = 8000
+
 # Production only
 load_dotenv('.env')
 
-wsgi_app = 'wsgi:app'
-bind = f'{os.getenv('APP_HOST')}:{os.getenv('APP_PORT')}'
+wsgi_app = 'wsgi'
+bind = f"{os.getenv('APP_HOST', DEFAULT_HOST)}:{os.getenv('APP_PORT', DEFAULT_PORT)}"
 workers = multiprocessing.cpu_count()
 threads = 2
 worker_class = 'gthread'
 log_level = 'info'
 reload = True
 preload_app = True
+daemon = True
